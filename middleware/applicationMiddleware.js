a23f6912fb79a7abb9a52846db771abbb9aab7c1
jeni1402 <paul.jenifer@datasirpi.com>
2026-04-25 15:17:17 +0530

    request & response

--- middleware/applicationMiddleware.js
+++ middleware/applicationMiddleware.js
@@ -5,17 +5,35 @@ import express from 'express';
 const app=express(); 
 const PORT =3000;
 
-// app.use((req,res,next)=>{
-//     console.log('time',Date.now);
-//     next();
+// Third party middleware
+import cookieParser from 'cookie-parser';
 
-// })
-app.get('/user',(req,res,next)=>{
-    res.send('user page ')
-    console.log("User is working");
-
+const time=((req,res,next)=>{
+    req.time=Date.now()
+    next();
 
 })
+
+app.use(cookieParser())
+// 
+app.get('/user',(req,res,next)=>{
+    res.send(String(req.url));
+})
+
+app.get('/student',(req,res,next)=>{
+    res.send("It is student page")
+    
+})
+
+app.get('/set-cookie', (req, res) => {
+    res.cookie('name', 'Paul');
+    res.send('Cookie set');
+});
+
+// Example: read cookie
+app.get('/get-cookie', (req, res) => {
+    res.send(`Cookie: ${req.cookies.name}`);
+});
 // error handling -middleware
 app.use((err,req,res,next)=>{
     console.log("Error ",err);
@@ -24,5 +42,6 @@ app.use((err,req,res,next)=>{
 })
 app.listen(PORT ,function(err){
     if(err) console.log(err);
-    console.log("Server is running on Port :",PORT)
+    console.log(`Server is running on http://localhost:${PORT} :`)
+    app.get(PORT);
 })
